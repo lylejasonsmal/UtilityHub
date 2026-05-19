5654952dcaf57eadd7415a60977732396de343da
Lyle Smal - Onsight <lyle@onsightapp.com>
2026-05-19 18:23:17 +0200

    Updates

--- src/components/commonly_used/MaterialDesignIcon.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <span class="material-symbols-outlined">{{params.icon}}</span>
-</template>
-<script setup>
-const params = defineProps({
-  icon: String,
-})
-</script>
--- src/pages/error_handling/PageNotFound.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1 class="large-text">Page Not Found</h1>
-</template>
